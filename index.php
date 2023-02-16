2de8ba60bedbcb9a4b14cd55d6270dd1f777b96e
mytory <mytory@gmail.com>
2023-02-17 01:51:00 +0900

    커맨드라인에서 인자값 받아서 API 통신을 할 수 있게 만들었음.

--- index.php
+++ index.php
@@ -4,9 +4,21 @@ use GuzzleHttp\Client;
 require 'vendor/autoload.php';
 require 'config.php';
 
+if ($argc != 6) {
+    echo "usage) php {$argv[0]} {prompt} {max_tokens} {temperature} {top_p} {n}\n";
+    echo "ex) php {$argv[0]} '오늘은 날씨가 맑습니다' 200 0.1 0.1 1\n";
+    exit;
+}
+
+$prompt = (string) $argv[1];
+$max_tokens = (int) $argv[2];
+$temperature = (float) $argv[3];
+$top_p = (float) $argv[4];
+$n = (int) $argv[5];
+
 $client = new Client([
     'base_uri' => 'https://api.kakaobrain.com/v1/inference/kogpt/generation',
-    'timeout'  => 30.0,
+    'timeout'  => 60.0,
 ]);
 
 $response = $client->request('POST', '', [
@@ -15,11 +27,11 @@ $response = $client->request('POST', '', [
         'Content-Type' => 'application/json',
     ],
     'json' => [
-        'prompt' => '오늘은 날씨가 맑습니다.',
-        'max_tokens' => 500,
-        'temperature' => 0.1,
-        'top_p' => 0.1,
-        'n' => 1
+        'prompt' => $prompt,
+        'max_tokens' => $max_tokens,
+        'temperature' => $temperature,
+        'top_p' => $top_p,
+        'n' => $n,
     ]
 ]);
 
